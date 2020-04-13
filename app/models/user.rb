e56a6f98116a291a7bc21ac66c171157e7c73a27
Jennifer Stephan <jennifer.stephan@edu.escpeurope.eu>
2020-04-13 13:05:44 +0200

    formulaire sign up ajout dattributs + design

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,5 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  attr_accessor :email, :password, :password_confirmation, :remember_me, :name, :familyname
 end
