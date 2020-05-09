de4ebe1e555ebd2353cf90a9f02a41925624df24
Marie-Noelle Fromage <fromage.marienoelle@gmail.com>
2020-05-09 17:46:28 +0200

    add security features in Heb Model with validation on presence and inclusion in array

--- app/models/heb.rb
+++ app/models/heb.rb
@@ -1,4 +1,15 @@
 class Heb < ApplicationRecord
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
+
+  SECURITY_FEATURES = [
+    "code à l'entrée",
+    "caméra de surveillance 24h/24",
+    "gardien 24h/24",
+    "gardien à horaires définis",
+    "autres"
+    ]
+
+  validates :security_features, presence: true, inclusion: { in: SECURITY_FEATURES }, on: :create
+
 end
