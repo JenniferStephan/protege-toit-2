b4c70a155a3dfcb1f5da144cb695497053050611
Marie-Noelle Fromage <fromage.marienoelle@gmail.com>
2020-05-09 17:54:07 +0200

    add avaibility to Hebs

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_09_154811) do
+ActiveRecord::Schema.define(version: 2020_05_09_155303) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 2020_05_09_154811) do
     t.integer "rooms_available", default: 0
     t.string "security_features"
     t.string "services"
+    t.boolean "availability", default: true
     t.index ["user_id"], name: "index_hebs_on_user_id"
   end
 
